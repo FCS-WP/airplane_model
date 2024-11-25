b4f150291d0a6d2c60555c608a905778841d1184
DevTran <tan.tran@floatingcube.com>
2024-11-25 16:20:40 +0700

    update function limit quanity product

--- src/wp-content/themes/weiboo-child/includes/shin_woo.php
+++ src/wp-content/themes/weiboo-child/includes/shin_woo.php
@@ -37,4 +37,44 @@ if (! function_exists('shin_change_currency_symbol')) {
 }
 
 
-function enqueue_wc_cart_fragments() { wp_enqueue_script( 'wc-cart-fragments' ); } add_action( 'wp_enqueue_scripts', 'enqueue_wc_cart_fragments' );
+add_filter('woocommerce_add_to_cart_validation', 'limit_quanity_products', 10, 3);
+function limit_quanity_products($passed, $product_id, $quantity) {
+
+    $cart_contents = WC()->cart->get_cart();
+    
+
+    foreach ($cart_contents as $cart_item) {
+        if ($cart_item['product_id'] == $product_id) {
+            $current_quantity = $cart_item['quantity']; 
+            $new_quantity = $current_quantity + $quantity;
+            
+            
+            if ($new_quantity > 6) {
+                wc_add_notice(__('You can only buy a maximum of 6 of this product.', 'woocommerce'), 'error');
+                return false;
+            }
+        }
+    }
+    
+
+    if ($quantity > 6) {
+        wc_add_notice(__('You can only buy a maximum of 6 of this product.', 'woocommerce'), 'error');
+        return false; 
+    }
+
+    return $passed; 
+}
+
+add_action('woocommerce_before_calculate_totals', 'limit_quanity_products_update_cart', 10, 1);
+function limit_quanity_products_update_cart($cart) {
+    
+    foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+        $quantity = $cart_item['quantity'];
+
+        if ($quantity > 6) {
+            $cart_item['quantity'] = 6; 
+            WC()->cart->set_quantity($cart_item_key, 6); 
+            wc_add_notice(__('You can only buy a maximum of 6 of each products.', 'woocommerce'), 'error');
+        }
+    }
+}
