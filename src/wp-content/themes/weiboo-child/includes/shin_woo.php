7b7d41c3c9ff2e9466d0ba3bbc5c8b4515df39b2
shin <nhathanh024@gmail.com>
2024-12-06 13:33:29 +0700

    hiden product private

--- src/wp-content/themes/weiboo-child/includes/shin_woo.php
+++ src/wp-content/themes/weiboo-child/includes/shin_woo.php
@@ -38,46 +38,61 @@ if (! function_exists('shin_change_currency_symbol')) {
 
 //check after click add to cart
 add_filter('woocommerce_add_to_cart_validation', 'limit_quanity_products', 10, 3);
-function limit_quanity_products($passed, $product_id, $quantity) {
+function limit_quanity_products($passed, $product_id, $quantity)
+{
 
     $cart_contents = WC()->cart->get_cart();
-    
+
     if ($quantity > 6) {
         wc_add_notice(__('You can only buy a maximum of 6 of this product.', 'woocommerce'), 'error');
-        return false; 
+        return false;
     }
 
-    
+
     foreach ($cart_contents as $cart_item) {
         if ($cart_item['product_id'] == $product_id) {
-            $current_quantity = $cart_item['quantity']; 
+            $current_quantity = $cart_item['quantity'];
             $new_quantity = $current_quantity + $quantity;
-            
-            
+
+
             if ($new_quantity > 6) {
                 wc_add_notice(__('You can only buy a maximum of 6 of this product.', 'woocommerce'), 'error');
                 return false;
             }
         }
     }
-    
 
-    
 
-    return $passed; 
+
+
+    return $passed;
 }
 
 //check after click update cart in cart page
 add_action('woocommerce_before_calculate_totals', 'limit_quanity_products_update_cart', 10, 1);
-function limit_quanity_products_update_cart($cart) {
-    
+function limit_quanity_products_update_cart($cart)
+{
+
     foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
         $quantity = $cart_item['quantity'];
 
         if ($quantity > 6) {
-            $cart_item['quantity'] = 6; 
-            WC()->cart->set_quantity($cart_item_key, 6); 
+            $cart_item['quantity'] = 6;
+            WC()->cart->set_quantity($cart_item_key, 6);
             wc_add_notice(__('You can only buy a maximum of 6 of each products.', 'woocommerce'), 'error');
         }
     }
+}
+
+/**
+ * Hide Product Has Status Private
+ *
+ */
+add_filter('posts_where', 'hide_private_products');
+
+function hide_private_products($where)
+{
+    if (is_admin()) return $where;
+    global $wpdb;
+    return " $where AND {$wpdb->posts}.post_status != 'private' ";
 }
