c9964f6b7bfdf6f0aa096b96b3af38c02db73d4c
shin <nhathanh024@gmail.com>
2024-12-13 11:18:44 +0700

    update filter featured

--- src/wp-content/themes/weiboo-child/includes/shin_woo.php
+++ src/wp-content/themes/weiboo-child/includes/shin_woo.php
@@ -180,3 +180,51 @@ function zippy_bank_details($order_id = '')
         <strong style="color:red;">Note:</strong> Click <a id="downloadBtn" style=" color: #42a2cd; font-weight: 700; " href="#"> here </a> to save the QR code for payment</p>';
         }
     }
+
+    add_filter('woocommerce_products_admin_list_table_filters', 'zippy_featured_filter');
+
+    function zippy_featured_filter($filters)
+    {
+        $filters['featured_choice'] = 'zippy_filter_by_featured';
+        return $filters;
+    }
+
+    function zippy_filter_by_featured()
+    {
+        $current_featured_choice = isset($_REQUEST['featured_choice']) ? wc_clean(wp_unslash($_REQUEST['featured_choice'])) : false;
+        $output = '<select name="featured_choice" id="dropdown_featured_choice"><option value="">Filter by featured status</option>';
+        $output .= '<option value="onlyfeatured" ';
+        $output .= selected('onlyfeatured', $current_featured_choice, false);
+        $output .= '>Featured Only</option>';
+        $output .= '<option value="notfeatured" ';
+        $output .= selected('notfeatured', $current_featured_choice, false);
+        $output .= '>Not Featured</option>';
+        $output .= '</select>';
+        echo $output;
+    }
+
+    add_filter('parse_query', 'zippy_featured_products_query');
+
+    function zippy_featured_products_query($query)
+    {
+        global $typenow;
+        if ($typenow == 'product') {
+            if (! empty($_GET['featured_choice'])) {
+                if ($_GET['featured_choice'] == 'onlyfeatured') {
+                    $query->query_vars['tax_query'][] = array(
+                        'taxonomy' => 'product_visibility',
+                        'field' => 'slug',
+                        'terms' => 'featured',
+                    );
+                } elseif ($_GET['featured_choice'] == 'notfeatured') {
+                    $query->query_vars['tax_query'][] = array(
+                        'taxonomy' => 'product_visibility',
+                        'field' => 'slug',
+                        'terms' => 'featured',
+                        'operator' => 'NOT IN',
+                    );
+                }
+            }
+        }
+        return $query;
+    }
